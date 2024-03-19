c8e6465c0f8659ed3677d3c641ce5b2fc417bb56
Diego Santana <3c.santana@gmail.com>
2024-03-19 12:48:24 -0300

    deletando dados com o JDBC

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,13 +1,35 @@
 package app;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnectionDb();
-        DB.closeConcection();
+        Connection conn = null;
+        PreparedStatement preparedSt = null;
+        
+        try {
+            conn = DB.getConnectionDb();
+            
+
+            preparedSt = conn.prepareStatement(
+                    "DELETE FROM department " +
+                            "WHERE Id = ?"
+            );
+            
+            preparedSt.setInt(1, 3);
+            int changedRows = preparedSt.executeUpdate();
+
+            System.out.println("Changed rows: " + changedRows);
+            
+            
+        }catch (SQLException e) {
+            throw  new DbException(e.getMessage());
+        }
 
         System.out.println("Terminei");
         
